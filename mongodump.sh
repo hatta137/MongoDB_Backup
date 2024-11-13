cf71e11b3c9c615669f7ce010aa76293ccd2a118
hendrik_wla <hendrik.lendeckel@fh-erfurt.de>
2024-11-13 13:33:36 +0100

    feat: using variables for better understanding

--- mongodump.sh
+++ mongodump.sh
@@ -1,1 +1,5 @@
-docker compose exec -T database sh -c 'mongodump --archive' > db.dump
+#!/bin/bash
+DB_CONTAINER="database"
+DB_PATH="db.dump"
+
+docker compose exec -T $DB_CONTAINER sh -c 'mongodump --archive' > DB_PATH
--- mongorestore.sh
+++ mongorestore.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DB_CONTAINER="database"
+DB_NAME="user"
+DB_PATH="db.dump"
 
 # Entfernt die bestehende Datenbank und importiert dann den Dump
-docker compose exec -T database sh -c "mongosh --eval 'db.getSiblingDB(\"user\").dropDatabase()' && mongorestore --archive" < db.dump
+docker compose exec -T $DB_CONTAINER sh -c "mongosh --eval 'db.getSiblingDB(\"$DB_DB_NAME\").dropDatabase()' && mongorestore --archive" < "$DB_PATH"
